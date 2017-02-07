20d9a75ea635a88665f3672c3f5b97278f0047e3
talum <tracy.a.lum@gmail.com>
2017-02-06 22:01:34 -0500

    add some controller tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -20,4 +20,39 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(302);
     }
+
+
+    public function testHomeWithAuth()
+    {
+        $user = factory(\App\User::class)->create();
+        $response = $this->actingAs($user)
+          ->withSession(['foo' => 'bar'])
+          ->get('/home');
+
+        $response->assertStatus(200);
+    }
+
+    public function testHomeWithoutAuth()
+    {
+          $response = $this->get('/home');
+
+        $response->assertStatus(302);
+    }
+
+    public function testSurveysWithAuth()
+    {
+        $user = factory(\App\User::class)->create();
+        $response = $this->actingAs($user)
+          ->withSession(['foo' => 'bar'])
+          ->get('/surveys');
+
+        $response->assertStatus(200);
+    }
+
+    public function testSurveysWithoutAuth()
+    {
+          $response = $this->get('/surveys');
+
+        $response->assertStatus(302);
+    }
 }
