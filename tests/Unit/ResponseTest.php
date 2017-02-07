6c4647e2cd684423be877a4850bc141bf50dfaa3
talum <tracy.a.lum@gmail.com>
2017-02-06 20:51:48 -0500

    add response test

---
+++ tests/Unit/ResponseTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Question;
+use App\Response;
+
+class ResponseTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testResponseConstructor()
+    {
+        $response = new Response(['content' => 'new response', 'survey_id' => 1]);
+        $this->assertEquals('new response', $response->content);
+    }
+    
+    public function testQuestionAssociation()
+    {
+        $question = Question::create(['content' => 'new question', 'survey_id' => 1]);
+        $response = Response::create(['content' => 'response 1', 'question_id' => $question->id]);
+        $this->assertEquals($question->content, $response->question->content);
+    }
+
+}
